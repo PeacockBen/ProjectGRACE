66905be44ea3ca5a4646c1d02840ae0646d49027
PeacockBen <100370850+PeacockBen@users.noreply.github.com>
2024-05-08 21:44:24 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,38 +1,8 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const Article = require('./models/articleModel');
-// const path = require('path');
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Connect to the database
-// mongoose.connect('mongodb://localhost:27017/ProjectGrace', { useNewUrlParser: true}).then(() => {
-//     console.log('Connected to MongoDB');
-// }).catch(err => {
-//     console.error('Error connecting to MongoDB:', err);
-// });
-
-// // Define a route
-// app.get('/articles', async (req, res) => {
-//     try{
-//         const articles = await Article.find();
-//         res.json(articles);
-//     }catch (error) {
-//         console.error('Error fetching articles:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-
 const express = require('express');
 const fs = require('fs')
 const path = require('path');
+const cron = require('node-cron');
+const { exec } = require('child_process');
 
 //  create express app
 const app = express();
@@ -52,7 +22,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/articles', (req, res) => {
-    res.json(articles);
+    fs.readFile('server/data/articles.json', 'utf8', (err, jsonData) => {
+        if (err) {
+            console.error('Error reading the articles file:', err);
+            res.status(500).send('Error reading articles data');
+            return;
+        }
+        res.json(JSON.parse(jsonData));
+    });
+});
+cron.schedule('0 22 * * *', function() {
+    console.log('Running a daily task to update articles.');
+    exec('python server/scheduler/main.py', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
 });
 
 app.listen(PORT, () => {
